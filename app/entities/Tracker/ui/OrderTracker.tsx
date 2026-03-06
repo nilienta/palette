6e6b58c5838ac8cc6c578eae1735450fbb3fc2e9
Anna <anml@yandex.ru>
2026-03-06 05:19:59 +0300

    change design OrderTracker

--- app/entities/Tracker/ui/OrderTracker.tsx
+++ app/entities/Tracker/ui/OrderTracker.tsx
@@ -1,6 +1,11 @@
-import { Form, Radio } from "antd";
 import { Order } from "~/shared/ui/Order/Order";
 import { useTracker } from "../hooks/useTracker";
+import { CoverSelector } from "~/shared/ui/CoverSelector/CoverSelector";
+import { Typography, Form, Switch } from "antd";
+import { ScissorOutlined } from "@ant-design/icons";
+import { Card } from "~/shared/ui/Order/Card";
+
+const { Text } = Typography;
 
 export const OrderTracker = () => {
   const { links, price, loading, error } = useTracker();
@@ -8,22 +13,28 @@ export const OrderTracker = () => {
   if (loading) return <div>Загрузка...</div>;
   if (error) return <div>Ошибка загрузки</div>;
 
-  const gameData = {
+  const trackerData = {
     name: "Трекер для раскрасок",
     price: price,
     imgSrc: links[0],
   };
 
   return (
-    <Order data={gameData}>
-      <Form.Item name="test">
-        <Radio.Group
-          options={[
-            { value: "yes", label: "Да" },
-            { value: "no", label: "Нет" },
-          ]}
-        />
-      </Form.Item>
+    <Order data={trackerData}>
+      <CoverSelector links={links} />
+      <Card title="Уточнения" icon={<ScissorOutlined />}>
+        <div className="flex flex-row justify-between items-center gap-2">
+          <div className="flex flex-col gap-1">
+            <Text strong>Разрезать наклейки</Text>
+            <Text style={{ fontSize: 12 }}>
+              Наклейки будут разрезаны по контуру
+            </Text>
+          </div>
+          <Form.Item name="cut">
+            <Switch defaultChecked />
+          </Form.Item>
+        </div>
+      </Card>
     </Order>
   );
-}
+};
